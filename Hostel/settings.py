1e82c895383d73b1d68f768db6d5d24e300e7e24
Sahaj Bamba <sahajbamba1999@gmail.com>
2019-04-23 23:50:41 +0530

    Updated settings.py

--- Hostel/settings.py
+++ Hostel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wim3ebayu)2mw*x$0iyg#cxkbjk049!vd-hoyb@26wsctwy59k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
