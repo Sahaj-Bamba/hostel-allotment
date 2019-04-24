81f87be96d800bc20ce34e541f285989c31554c1
Sahaj Bamba <sahajbamba1999@gmail.com>
2019-04-24 17:47:46 +0530

    somethings working Its great

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login, logout
-from django.contrib.auth.models import User
-from . import forms
-from .models import priority
 from .models import User_details
-from .models import user_requests
-from .models import Team_details
 from base.views import update
 from django.core.exceptions import ObjectDoesNotExist
 
--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'base'
 
 urlpatterns = [
-    re_path(r'', views.home, name='home'),
+    re_path(r'home/', views.home, name='home'),
     re_path(r'^[?]*', views.not_found, name='not_found'),
 ]
